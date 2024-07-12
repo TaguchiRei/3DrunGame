1da6efa33c0fdfbcc37b59f199787f62182a4a81
TaguchiRei <ren.len2426@gmail.com>
2024-07-12 16:09:33 +0900

    3Drungame製作途中

---
+++ Assets/GameManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static bool pause = false;
+    public static bool start =false;
+    // Update is called once per frame
+    void Update()
+    {
+        if (!start)
+        {
+            
+        }
+        else
+        {
+
+        }
+    }
+}
